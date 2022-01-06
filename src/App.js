c2edaf915d07974437ce1ba2882771065e8904cb
Paawan Singal <paawansingal09@gmail.com>
2022-01-06 10:58:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import About from "./components/About";
 import Alert from "./components/Alert";
 import { Navbar } from "./components/Navbar";
 import Textform from "./components/Textform";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   const [darkmode, setdarkmode] = useState("light")
@@ -17,8 +22,8 @@ function App() {
     })
 
     setTimeout(() => {
-        setalert({});
-    }, 2000);
+      setalert({});
+    }, 1000);
   }
 
   const togglemode = () => {
@@ -26,22 +31,25 @@ function App() {
       setdarkmode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode has been enabled", "success");
-  
+
     } else {
       setdarkmode("dark");
       document.body.style.backgroundColor = "grey";
       showAlert("Dark Mode has been enabled", "success");
-      
+
     }
   }
 
   return (
     <div>
-
-      <Navbar title="TextUtils" about="About" mode={darkmode} toggleMode={togglemode} />
-      <Alert alert={alert} />
-      <About />
-      <Textform showAlert = {showAlert} heading="Enter Your Text To Analyze" mode={darkmode} />
+      <Router>
+        <Navbar title="TextUtils" about="About" mode={darkmode} toggleMode={togglemode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/' element={<Textform showAlert={showAlert} heading="Enter Your Text To Analyze" mode={darkmode} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
